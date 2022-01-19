070fcb48faca4e54ede67c047a072e48e4d0098c
Guilllermo Oscar <85650228+GuillermoOsc@users.noreply.github.com>
2022-01-19 02:32:21 -0300

    cambios apps y vistas

--- project/core/views.py
+++ project/core/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def inicio(request):  #Solicitud (request) de la vista
+
+    return render(request, "core/inicio.html") #Respuesta del la vista html (templates)
--- project/project/urls.py
+++ project/project/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from core import views #se importa las vistas (views) desde la app core
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.inicio, name="Inicio"),
 ]
